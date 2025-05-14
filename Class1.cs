2a4e3cec2ecca31a3b04df499886abc0494add58
rfsampaio <rfsampaio@SAO170DT137509>
2025-05-14 10:01:30 -0300

    Adicionar arquivos de projeto.

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+
+    static void Main(string[] args)
+    {
+        string[] someWords =
+        {
+            "the",
+            "quick",
+            "brown",
+            "fox",
+            "jumps"
+        };
+
+        //string[] moreWords =
+        //{
+        //    "over",
+        //    "the",
+        //    "lazy",
+        //    "dog"
+        //};
+
+        IEnumerable<string> query = from word in someWords
+                                    orderby word
+                                    select word;
+
+        foreach (string str in query)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
